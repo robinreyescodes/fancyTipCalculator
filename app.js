bfaf980c6b1b2c59c89d7038d2f721086d73a0c3
Robin Reyes <robinreyes.code@gmail.com>
2022-12-07 10:55:49 -0800

    fixed amount/total per person bug, working now

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const billInput = document.querySelector("#bill-input");
 const peopleInput = document.querySelector("#people-input");
 const tipAmount = document.querySelector(".tip-amount");
 const totalAmount = document.querySelector(".total-amount");
-const tipBtns = document.querySelectorAll(".btn");
+// const tipBtns = document.querySelectorAll(".btn");
+const tipBtns = document.querySelector(".tip-btns");
 const aBtn = document.querySelector(".btn");
 const resetBtn = document.querySelector(".reset-btn");
-let tipPercent = 0;
+let tipPerPerson = 0;
+let tipTotal = 0;
 let billValue = 0;
 let peopleValue = 0;
 let totalValue = 0;
@@ -18,27 +20,28 @@ billInput.addEventListener("keyup", (e) => {
 });
 
 //gives clickability and function for each of the tip buttons
-tipBtns.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    tipBtns.forEach((btn) => btn.classList.remove("btn-active"));
-    btn.classList.toggle("btn-active");
-    //stores selected tip percentage in variable, turns to number form
-    tipPercent = Number(e.target.value.slice(0, -1));
-    //changes visual of tip amount
-    tipAmount.textContent = `$${((billValue * tipPercent) / 100).toFixed(2)}`;
-    totalValue = billValue + (billValue * tipPercent) / 100;
-  });
+
+tipBtns.addEventListener("click", (e) => {
+  //style changes when btn clicked
+  e.target.classList.toggle("btn-active");
+  //store tip percent in variable
+  tipTotal = billValue * Number(e.target.value.slice(0, -1) / 100);
+  console.log(tipTotal);
 });
 
 //gets how many ways the bill needs to be split in
 peopleInput.addEventListener("keyup", (e) => {
   peopleValue = Number(e.target.value);
-  totalAmount.textContent = `$${(totalValue / peopleValue).toFixed(2)}`;
+  tipPerPerson = tipTotal / peopleValue;
+  tipAmount.textContent = `$${tipPerPerson.toFixed(2)}`;
+  totalAmount.textContent = `$${((billValue + tipTotal) / peopleValue).toFixed(
+    2
+  )}`;
 });
 
 //resets all the values
 resetBtn.addEventListener("click", () => {
-  tipBtns.forEach((btn) => btn.classList.remove("btn-active"));
+  // tipBtns.forEach((btn) => btn.classList.remove("btn-active"));
   billInput.value = "";
   peopleInput.value = "";
   totalAmount.textContent = "$0.00";
